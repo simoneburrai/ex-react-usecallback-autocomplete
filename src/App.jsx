2ed85935dc0493f79527305db1c4aa77e2ecf892
simoneburrai <burrai.s98@gmail.com>
2025-07-28 12:31:41 +0200

    feat: milestone 2 debounce completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-import { useEffect, useState } from "react";
+// 📌 Milestone 2: Implementare il Debounce per Ottimizzare la Ricerca
+// Attualmente, ogni pressione di tasto esegue una richiesta API. Questo è inefficiente!
+// Implementa una funzione di debounce per ritardare la chiamata API fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
+// Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
 
+// Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
 
+import { useEffect, useState, useCallback } from "react";
+
+  const debounce= ((callback, delay)=>{
+    let timer;
+    return ()=>{
+      clearTimeout(timer);
+      setTimeout(()=>{
+        callback();
+      }, delay)
+    }
+  })
+  
 function App() {
 
   const apiUrl = "http://localhost:3333";
@@ -8,6 +24,8 @@ function App() {
   const [inputSearch, setInputSearch] = useState("");
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
+
+
 
   const searchQuery = async ()=>{
     const res = await fetch(`${apiUrl}${productQueryUrl}${inputSearch}`);
@@ -21,12 +39,14 @@ function App() {
     setProducts(data);
   }
 
+  const callBackSearch = useCallback(debounce(searchQuery, 500), [inputSearch]);
+
   useEffect(()=>{
     getProducts();
   },[])
 
   useEffect(()=>{
-    searchQuery();
+    callBackSearch();
   },[inputSearch])
 
 
@@ -38,7 +58,7 @@ function App() {
     <div className="d-flex justify-content-between text-white">
       <div className="m-3 results row row-cols-4 bg-primary w-70">
         {products.map(p=><div key={p.id} className="p-3">
-          <h3>{p.brand}</h3>
+          <h3>{p.name}</h3>
           <img src={p.image} alt={p.brand} />
           <h5>{p.price}</h5>
           <p>{p.description}</p>
@@ -50,7 +70,7 @@ function App() {
       <h3 className="w-100 text-center">Searched Products</h3>
       {filteredProducts.length === 0 && <h4 className="text-center">Nessun Prodotto Trovato</h4>}
         {filteredProducts.map(p=><div key={p.id} className="p-3">
-          <h3>{p.brand}</h3>
+          <h3>{p.name}</h3>
           <img src={p.image} alt={p.brand} />
           <h5>{p.price}</h5>
           <p>{p.description}</p>
